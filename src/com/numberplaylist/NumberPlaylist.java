392aa896d47d49bcb6bd1edac3ea13916689a8ea
akashgupta3852 <akashgupta3852@gmail.com>
2020-10-06 00:14:57 +0530

    [Akash]Added: forEach method with stream() to print all the elements

--- src/com/numberplaylist/NumberPlaylist.java
+++ src/com/numberplaylist/NumberPlaylist.java
@@ -1,10 +1,23 @@
 package com.numberplaylist;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class NumberPlaylist {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to Number Playlist Program");
+		
+		List<Integer> numberPlayList = new ArrayList<>();
+		System.out.println("Enter the size of the list:");
+		Scanner sc = new Scanner(System.in);
+		int size = sc.nextInt();
+		for(int i= 0; i<size; i++)
+			numberPlayList.add(i);
 
+		System.out.println("Elements in the list:");
+		numberPlayList.stream().forEach(n -> {System.out.println(n);});
 	}
 
 }
